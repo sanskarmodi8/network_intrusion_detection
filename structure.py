37c9589278fa7fea768138a2a08101bc0d691969
Sanskar Modi <sansyprog8@gmail.com>
2024-03-28 19:01:31 +0530

    initial commit- added README.md and created the project structure using the python script structure.py

---
+++ structure.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'NetworkIntrusionDetection'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/eda_and_feature_engineering.py",
+    f"src/{project_name}/components/prepare_model.py",
+    f"src/{project_name}/components/train_model.py",
+    f"src/{project_name}/components/evaluation.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/prediction.py",
+    f"src/{project_name}/pipeline/stage_01_data_ingestion.py",
+    f"src/{project_name}/pipeline/stage_02_eda_and_feature_engineering.py",
+    f"src/{project_name}/pipeline/stage_03_prepare_model.py",
+    f"src/{project_name}/pipeline/stage_04_training_model.py",
+    f"src/{project_name}/pipeline/stage_05_evaluation.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "main.py",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    ".gitignore",
+    ".dvcignore",
+    "app.py",
+    "Dockerfile"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    # create the directory if not exists 
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file: {filename}")
+        
+    # create the file if not exists 
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0 ):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Created empty file: {filepath}")
+            
+    else:
+        logging.info(f"File {filename} already exists")
